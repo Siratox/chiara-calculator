8ebe08452f773c9f889d7fcdb269e01d0ebf422b
Tim Kluge <ubrfu@student.kit.edu>
2025-03-12 15:07:08 +0100

    add html and css

---
+++ main.js
@@ -0,0 +1,20 @@
+let a = 15;
+let b = 8;
+
+function max(m, n) {
+  if (m > n) {
+    return m;
+  } else {
+    return n;
+  }
+}
+
+// let tim = 'tim ist schwul';
+
+// let number = parseInt(a);
+
+// console.log(a, number);
+
+// groessere variable ausgeben
+let result = max(a, b);
+console.log(result);
